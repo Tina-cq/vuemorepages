052cd24056e903949af50968972455f308661097
caoqin <qincao@beigebigdata.com>
2019-04-17 14:01:22 +0800

    多页面配置

---
+++ src/App02.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app02">
+    <img src="./assets/logo.png">App02
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App02'
+}
+</script>
+
+<style>
+#app02 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
